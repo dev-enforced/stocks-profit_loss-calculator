90d3cbeb5e75462372c1720bd1046b07e196b2a1
dev-enforced <devenforced@gmail.com>
2021-09-16 21:12:37 +0530

    refactor: added the no profit no loss case requirements

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var outputDiv=document.querySelector(".main-output");
 var outputMsg=document.querySelector("#main-msg");
 var outputImg=document.querySelector("#image-show");
 var body=document.querySelector("body");
-var status=0;
+var flag=0;
 function formResponseHandler(event){
     event.preventDefault();
     var cost=Number(inputs[0].value);
@@ -14,12 +14,12 @@ function formResponseHandler(event){
 }
 
 function show(stats,result,resPer){
-    if(stats==1){
+    if(stats===1){
         outputDiv.style.display="block";
         outputMsg.style.color='#d62828';
         outputMsg.innerText=`Unfortunately a total loss of ${result} units has been incurred in this scenario at a loss of ${resPer}%.`
         outputImg.src='https://apps.indianmoney.com/images/article-images/how-to-survive-in-stock-market-downfall.jpg?t=1629463197'
-    }else if(stats==2){
+    }else if(stats===2){
         outputDiv.style.display="block";
         outputMsg.style.color='#D9D9D9';
         outputMsg.innerText=`Congratulations a total profit of ${result} units has been earned in this scenario at a profit of ${resPer}%.`
@@ -34,15 +34,21 @@ function calculator(cp,number,sp){
         var totalLoss=loss*number;
         var lossInPercent=(loss*100)/cp;
         lossInPercent=lossInPercent.toFixed(2);
-        status=1;
-        show(status,totalLoss,lossInPercent);
-    }else{
+        flag=1;
+        show(flag,totalLoss,lossInPercent);
+    }else if(sp>cp){
         var profit=sp-cp;
         var totalProfit=profit*number;
         var profitInPercent=(profit*100)/cp;
         profitInPercent=profitInPercent.toFixed(2);
-        status=2;
-        show(status,totalProfit,profitInPercent);
+        flag=2;
+        show(flag,totalProfit,profitInPercent);
+    }else if(sp===cp){
+        var noReturn=sp-cp;
+        outputDiv.style.display="block";
+        outputMsg.style.color='#ffff';
+        outputMsg.innerText=` In this scenario no profit or loss has been received.`
+        outputImg.src='https://scontent.fbom37-1.fna.fbcdn.net/v/t1.18169-9/p240x240/1888517_1449560365347876_4970078213398423972_n.png?_nc_cat=109&ccb=1-5&_nc_sid=85a577&_nc_ohc=Bl3QQNuxb9EAX9dDMhT&_nc_ht=scontent.fbom37-1.fna&oh=b8b11cd8133cb656816cf176498315e7&oe=61672123'
     }
 }
 form.addEventListener('submit',(e)=>formResponseHandler(e))
